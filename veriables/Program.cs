cf7aeea079eefe996d8aaa451088d7ae8bf4d7dd
halit <halit@HALITTIRYAKICOM>
2024-07-16 20:52:43 +0300

    Add project files.

---
+++ veriables/Program.cs
@@ -0,0 +1,16 @@
+﻿int yas;
+yas = 28; // assignmet - set - atama işlemi
+Console.WriteLine(yas); // read - okuma işlemi
+
+char karakter = 'a'; //assignmet işlemi
+
+string karakterDizisi = "halit"; //assignmet işlemi
+
+Console.WriteLine("Lütfen isiminizi giriniz");
+karakterDizisi  = Console.ReadLine(); //assignmet işlemi
+Console.WriteLine("Değişken içerisinde veri --->" + karakterDizisi);//read
+
+Console.WriteLine("<----------------------------------------------------------------->");
+Console.Write("lütfen yaşını gir gardaşşşş: ");
+int yas2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("kişinin yaşı: "+yas2);
